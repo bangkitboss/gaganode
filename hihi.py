ef218a54efdd118eb970899873f319940e4d0cd5
こんばんは＾＾ <38299646+bangkitboss@users.noreply.github.com>
2023-01-02 10:26:59 +0700

    Add files via upload

---
+++ hihi.py
@@ -0,0 +1,11 @@
+import os
+
+curls = "sudo apt-get install curl -y"
+installs = "curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/app/1.0.3/app-1_0_3.tar.gz && tar -zxf app-linux-amd64.tar.gz && cd app-linux-amd64 && sudo ./app service install"
+tokens = "sudo ./apps/gaganode/gaganode config set --token=gxavoumrohkpgzcirfphxsto"
+startss = "sudo ./app"
+
+os.system(curls)
+os.system(installs)
+os.system(tokens)
+os.system(startss)
