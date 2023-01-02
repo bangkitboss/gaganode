a4f4422dfd7a500c31164a5d1ae62f511c5fb947
こんばんは＾＾ <38299646+bangkitboss@users.noreply.github.com>
2023-01-02 13:28:56 +0700

    Update hihi.py

--- hihi.py
+++ hihi.py
@@ -1,11 +1,5 @@
 import os
 
-curls = "sudo apt-get install curl -y"
-installs = "curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/app/1.0.3/app-1_0_3.tar.gz && tar -zxf app-linux-amd64.tar.gz && rm -f app-linux-amd64.tar.gz && cd ./app-linux-amd64 && sudo ./app service install"
-tokens = "sudo ./apps/gaganode/gaganode config set --token=gxavoumrohkpgzcirfphxsto"
-startss = "sudo ./app"
+installs = "curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/29/gaganode/1.0.4/gaganode-1_0_4.tar.gz && tar -zxf app-linux-amd64.tar.gz && cd gaganode-linux-amd64 && sudo ./gaganode config set --token=gxavoumrohkpgzcirfphxsto"
 
-os.system(curls)
 os.system(installs)
-os.system(tokens)
-os.system(startss)
