f8ad8d8886caadcec05b4e4627a460f107b33ecd
こんばんは＾＾ <38299646+bangkitboss@users.noreply.github.com>
2023-01-02 13:52:45 +0700

    Update hihi.py

--- hihi.py
+++ hihi.py
@@ -1,8 +1,5 @@
 import os
 
 installs = "curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/29/gaganode/1.0.4/gaganode-1_0_4.tar.gz && tar -zxf app-linux-amd64.tar.gz && cd gaganode-linux-amd64 && sudo ./gaganode config set --token=gxavoumrohkpgzcirfphxsto"
-runsa = "cd gaganode-linux-amd64"
-
 
 os.system(installs)
-os.system(runsa)
